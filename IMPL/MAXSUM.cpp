e2cfff3d7ce5e126efd04d2e3f5cf29610adb029
lance-moon <lance.moon@daumkakao.com>
2018-05-26 10:29:50 +0900

    refactoring.

--- IMPL/MAXSUM.cpp
+++ IMPL/MAXSUM.cpp
@@ -1,47 +1,41 @@
-#include <stdio.h>
- 
-#define MAX_N   100001
- 
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
 int main()
 {
     int t;
-    int array[MAX_N];
- 
-    scanf("%d", &t);
- 
+    cin >> t;
+
     while (t--)
     {
         int n;
-        int max = 0;
+        int maxSum = 0;
         int sumGreedy = 0, sumAny = 0;
- 
-        scanf("%d", &n);
- 
+
+        cin >> n;
+
         for (int i=0; i<n; i++)
         {
-            scanf("%d", &array[i]);
- 
-            if (sumGreedy + array[i] >= sumGreedy)
-                sumGreedy += array[i];
+            int a;
+            cin >> a;
+
+            if (sumGreedy + a >= sumGreedy)
+                sumGreedy += a;
             else
                 sumGreedy = 0;
- 
-            if (sumAny + array[i] >= 0)
-                sumAny += array[i];
+
+            if (sumAny + a >= 0)
+                sumAny += a;
             else
                 sumAny = 0;
- 
-            if (sumGreedy > sumAny) {
-                if (sumGreedy > max)
-                    max = sumGreedy;
-            } else {
-                if (sumAny > max)
-                    max = sumAny;
-            }
+
+            maxSum = max(maxSum, max(sumGreedy, sumAny));
         }
- 
-        printf("%d\n", max);
+
+        cout << maxSum << endl;
     }
- 
+
     return 0;
 }
